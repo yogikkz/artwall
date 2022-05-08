0933a28071eb3a6b20623e3e9fde86a39db82824
Jerzy Woźniak <yoogiman@gmail.com>
2022-05-08 10:50:42 +0200

    add gallery, text block, icon cards and footer - only mobile

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,16 +1,10 @@
 const burger = document.querySelector('.burger')
 const navBurger = document.querySelector('.nav__burger')
 const burgerIcon = burger.querySelectorAll('i')
-const navBurgerTitle = navBurger.getElementsByTagName('h2')
-const navBurgerLink = navBurger.querySelectorAll('ul li ul')
+const navBurgerTitle = navBurger.getElementsByTagName('ul li')
+const navBurgerLink = navBurger.getElementsByTagName('ul li ul')
 
 burger.addEventListener('click', () => {
 	navBurger.classList.toggle('open')
 	burgerIcon.forEach(element => element.classList.toggle('active'))
 })
-
-for (i = 0; i < navBurgerTitle.length; i++) {
-	navBurgerTitle[i].addEventListener('click', function () {
-		this.classList.toggle('show')
-	})
-}
