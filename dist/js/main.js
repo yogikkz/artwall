a2c3f6755d9a884ad9fec840ab875acd35be6c46
Jerzy Woźniak <yoogiman@gmail.com>
2022-05-09 13:31:35 +0200

    add cookie bar

--- dist/js/main.js
+++ dist/js/main.js
@@ -28,3 +28,34 @@ for (i = 0; i < searchBtn.length; i++) {
 		searchBar.addEventListener('keyup', enterKeyCheck)
 	})
 }
+
+// cookies
+
+setCookie = (cName, cValue, expDays) => {
+	let date = new Date()
+	date.setTime(date.getTime() + expDays * 24 * 60 * 60 * 1000)
+	const expires = 'expires=' + date.toUTCString()
+	document.cookie = cName + '=' + cValue + '; ' + expires + '; path=/'
+}
+
+getCookie = cName => {
+	const name = cName + '='
+	const cDecoded = decodeURIComponent(document.cookie)
+	const cArr = cDecoded.split('; ')
+	let value
+	cArr.forEach(val => {
+		if (val.indexOf(name) === 0) value = val.substring(name.lenght)
+	})
+	return value
+}
+
+document.querySelector('.cookies-btn').addEventListener('click', () => {
+	document.querySelector('.cookies').style.display = 'none'
+	setCookie('cookie', true, 30)
+})
+
+cookieMessage = () => {
+	if (!getCookie('cookie')) document.querySelector('.cookies').style.display = 'block'
+}
+
+window.addEventListener('load', cookieMessage)
