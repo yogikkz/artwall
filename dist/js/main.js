f16c38cde60997674db812cf32bae5d0adafb467
Jerzy Woźniak <yoogiman@gmail.com>
2022-05-09 08:20:12 +0200

    add scroll down to nav-bar links

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,10 +1,16 @@
 const burger = document.querySelector('.burger')
-const navBurger = document.querySelector('.nav__burger')
+const navBurger = document.querySelector('.nav-burger')
 const burgerIcon = burger.querySelectorAll('i')
-const navBurgerTitle = navBurger.getElementsByTagName('ul li')
-const navBurgerLink = navBurger.getElementsByTagName('ul li ul')
 
 burger.addEventListener('click', () => {
 	navBurger.classList.toggle('open')
 	burgerIcon.forEach(element => element.classList.toggle('active'))
 })
+
+const navBurgerLink = navBurger.getElementsByClassName('burger-link')
+
+for (i = 0; i < navBurgerLink.length; i++) {
+	navBurgerLink[i].addEventListener('click', function () {
+		this.classList.toggle('active')
+	})
+}
